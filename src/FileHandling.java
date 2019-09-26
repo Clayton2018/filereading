6079fab608b322de2691850c7a575cc63ca0d263
Clayton <cmpetersen@hotmail.co.za>
2019-09-26 08:15:58 +0200

    added file handling methods

--- src/FileHandling.java
+++ src/FileHandling.java
@@ -1,17 +1,51 @@
-import java.io.BufferedReader;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
 public class FileHandling {
+
+    private static ArrayList<String> employees = new ArrayList<>();
 
     public static void main(String[] args) {
 
         displayEmployees();
         readFile();
+        writeFile();
 
     }
+
+    private static void writeFile() {
+
+        Path path = Paths.get("C:/Users/TRAIN/Downloads/Clayton Oracle Java programs/Java Programming 2019 Learner/userNames.txt");
+
+        try{
+
+            BufferedWriter bw = Files.newBufferedWriter(
+                    path, Charset.forName("ISO-8859-1"),
+                    StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            for (String employee: employees){
+
+                bw.write(employee);
+                bw.newLine();
+
+            }//end for
+
+            bw.close();
+
+        }//end try
+
+        catch (IOException ioe){
+
+            System.out.println("Error reading file");
+            System.exit(0);
+
+        }//end catch
+
+    }//end method writeFile
 
     private static void readFile() {
 
@@ -34,11 +68,33 @@ public class FileHandling {
 
         }//end try
 
+        catch(FileNotFoundException fnfe){
+
+            System.out.println("File not found");
+
+        }//end FileNotFoundException
+
+        catch (EOFException eofe){
+
+            System.out.println("End of file! no more lines to read!");
+
+        }//end EOFException
+
+        catch (IOException ioe){
+
+            System.out.println("Error reading file!");
+
+        }//end IOException
+
     }//end readFile
 
     private static void displayEmployees() {
 
+        for(String employee: employees) {
 
+            System.out.println(employee);
+
+        }//end for loop
 
     }//end displayEmployees
 
